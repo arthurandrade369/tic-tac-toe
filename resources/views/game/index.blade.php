3f70442e79c5778064340665654af773e7515db3
Arthur Andrade <arthurandrade137@gmail.com>
2024-07-26 13:22:34 -0300

    Add new assets and update views

--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -1,7 +1,29 @@
 @extends('layouts.template')
 
+@push('scripts')
+<script src="{{ asset('js/script.js') }}"></script>
+@endpush
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+@endpush
+
 @section('content')
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="container-md bg-purple">
+        <div class="row justify-content-center">
+            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+        </div>
+    </div>
 @endsection
