6dbd2bdef9d3937116ca9a15673fe71682d7be83
Arthur Andrade <arthurandrade137@gmail.com>
2024-07-30 00:58:52 -0300

    Add package-lock.json and update project dependencies Remove public/css/style.css, public/js/script.js Update resources/css/app.css, resources/js/app.js Update resources/views/game/index.blade.php, resources/views/layouts/template.blade.php Update vite.config.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,36 @@
 import './bootstrap';
+const crossRect = 'imgs/rectangle-cross.svg';
+const circleRect = 'imgs/rectangle-circle.svg';
+const rects = handleRects();
+
+function handleRects() {
+    let rects = document.getElementsByClassName("rect-svg");
+    let initedRect = [];
+    
+    for (let rect of rects) {
+        fillRects(initedRect, rect)
+    }
+
+    createRectsEvents(initedRect);
+    return initedRect;
+}
+
+function fillRects(obj, rect) {
+    obj.push({
+        id: rect.id,
+        element: rect,
+        src: rect.src,
+        filledSrc: null,
+        isFilled: false
+    });
+}
+
+function createRectsEvents(rects) {
+    rects.forEach(rect => {
+        rect.element.addEventListener("click", handleRectClick(rect));
+    });
+}
+
+function handleRectClick(rect) {
+    
+}
--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.template')
 
 @push('scripts')
-<script src="{{ asset('js/script.js') }}"></script>
+@vite('resources/js/app.js')
 @endpush
 
 @push('styles')
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    @vite('resources/css/app.css')"
 @endpush
 
 @section('content')
     <div class="container-md bg-purple">
         <div class="row justify-content-center">
-            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="11" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="12" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="13" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="21" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="22" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="23" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img class="empty-rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="31" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="32" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="33" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
         </div>
     </div>
 @endsection
