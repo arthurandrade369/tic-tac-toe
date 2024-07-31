1ab49b648b5259a0cc5fd369f01d049992825f88
Arthur Andrade <arthurandrade137@gmail.com>
2024-07-31 00:49:29 -0300

    WIP on app.js: Adding the win coditions.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,17 @@ import './bootstrap';
 const crossRect = 'imgs/rectangle-cross.svg';
 const circleRect = 'imgs/rectangle-circle.svg';
 const rects = handleRects();
+let gameTurn = 'X';
+const winCombinations = [
+    ['11', '12', '23'], ['21', '22', '23'], ['31', '32', '33'],
+    ['11', '22', '33'], ['13', '22', '31'], ['11', '21', '31'],
+    ['12', '22', '32'], ['13', '23', '33']
+];
 
 function handleRects() {
     let rects = document.getElementsByClassName("rect-svg");
     let initedRect = [];
-    
+
     for (let rect of rects) {
         fillRects(initedRect, rect)
     }
@@ -27,10 +33,39 @@ function fillRects(obj, rect) {
 
 function createRectsEvents(rects) {
     rects.forEach(rect => {
-        rect.element.addEventListener("click", handleRectClick(rect));
+        rect.element.addEventListener("click", rectClick);
     });
 }
 
-function handleRectClick(rect) {
-    
+function rectClick(e) {
+    let rect = rects.find(rect => rect.id === e.target.id);
+    handleGameTurn(rect);
+}
+
+function handleGameTurn(rect) {
+    if (rect.isFilled) {
+        return;
+    }
+
+    let img = gameTurn === 'X' ? crossRect : circleRect;
+    rect.element.src = img;
+    rect.filledSrc = gameTurn;
+    rect.isFilled = true;
+    checkWinner();
+    gameTurn = gameTurn === 'X' ? 'O' : 'X';
+}
+
+function checkWinner() {
+    winCombinations.forEach(combination => {
+        let firstRect = rects.find(rect => rect.id === combination[0]);
+        let secondRect = rects.find(rect => rect.id === combination[1]);
+        let thirdRect = rects.find(rect => rect.id === combination[2]);
+
+        if (firstRect.isFilled && secondRect.isFilled && thirdRect.isFilled) {
+            if (firstRect.filledSrc === secondRect.filledSrc && firstRect.filledSrc === thirdRect.filledSrc) {
+                alert("Winner is " + gameTurn);
+                location.reload();
+            }
+        }
+    });
 }
