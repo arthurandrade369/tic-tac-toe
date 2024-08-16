c028b9e16a9facff9432b47a2f166d19a7802039
Arthur Andrade <arthurandrade137@gmail.com>
2024-08-16 13:16:19 -0300

    feat: Add sidebar to the game index page

--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.template')
 
 @push('scripts')
-@vite('resources/js/app.js')
+    @vite('resources/js/app.js')
 @endpush
 
 @push('styles')
@@ -11,19 +11,28 @@
 @section('content')
     <div class="container-md bg-purple">
         <div class="row justify-content-center">
-            <div class="col-2 px-0 py-3 text-center"><img id="11" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img id="12" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img id="13" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="11" class="rect-svg"
+                    src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="12" class="rect-svg"
+                    src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="13" class="rect-svg"
+                    src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-2 px-0 py-3 text-center"><img id="21" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img id="22" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img id="23" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="21" class="rect-svg"
+                    src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="22" class="rect-svg"
+                    src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="23" class="rect-svg"
+                    src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-2 px-0 py-3 text-center"><img id="31" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img id="32" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
-            <div class="col-2 px-0 py-3 text-center"><img id="33" class="rect-svg" src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="31" class="rect-svg"
+                    src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="32" class="rect-svg"
+                    src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
+            <div class="col-2 px-0 py-3 text-center"><img id="33" class="rect-svg"
+                    src="{{ asset('imgs/rectangle-empty.svg') }}" alt="Empty rectangle"></div>
         </div>
     </div>
 @endsection
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,43 +1,191 @@
-<aside class="navbar bg-body-tertiary fixed-top max-widht: 15%">
-    {{-- <div class="container-fluid">
-        <a class="navbar-brand" href="#">Offcanvas navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div> --}}
-            <div >
-                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="d-flex mt-3" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        {{-- </div> --}}
-    {{-- </div> --}}
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="/" class="brand-link text-center">
+        <img src="#" alt="LogoTTT" class="brand-image">
+        <span class="brand-text font-weight-light">TicTacToe</span>
+    </a>
+    <div class="sidebar">
+        <div class="form-inline" style="margin-top: 10px;">
+        </div>
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon far fa-file-alt"></i>
+                        <p>
+                            Inscrições
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon far fa-file-excel"></i>
+                        <p>
+                            Relatórios
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fa fa-chart-line"></i>
+                        <p>
+                            Estatísticas
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-upload"></i>
+                        <p>
+                            Importações
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-table"></i>
+                        <p>
+                            Quadro de Vagas
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-list"></i>
+                        <p>
+                            Chamadas
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-sort-amount-up"></i>
+                        <p>
+                            Classificação
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-solid fa-stamp"></i>
+                        <p>
+                            Pré-matrículas
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-file-alt"></i>
+                        <p>
+                            Avaliações
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-users-slash"></i>
+                        <p>
+                            Recursos
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-file-contract"></i>
+                        <p>
+                            Recursos contra a prova
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon far fa-edit"></i>
+                        <p>
+                            Configurações
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-stream"></i>
+                        <p>
+                            Triênios
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-school"></i>
+                        <p>
+                            Escolas
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-map"></i>
+                        <p>
+                            Locais de Prova (Cidades)
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-history"></i>
+                        <p>
+                            Logs
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>
+                            Usuários
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-user-shield"></i>
+                        <p>
+                            Administradores
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon far fa-newspaper"></i>
+                        <p>
+                            Perfis
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-paperclip"></i>
+                        <p>
+                            Permissões
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-university"></i>
+                        <p>
+                            Campi
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-list-ul"></i>
+                        <p>
+                            Cursos
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </aside>
